4e3f75a7cc9a8e471ff0e543bc23c1498f5b7276
Rowan <shepherd.rowan.k@gmail.com>
2021-05-10 16:37:21 -0700

    Created functions to choose computer option

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const game = () => {
 	let pScore = 0;
 	let cScore = 0;
 
+	//Start the game
+
 	const startGame = () => {
 		const playBtn = document.querySelector('.intro button');
 		const introScreen = document.querySelector('.intro');
@@ -9,10 +11,30 @@ const game = () => {
 
 		playBtn.addEventListener('click', () => {
 			introScreen.classList.add('fadeOut');
+			match.classList.add('fadeIn');
+		});
+	};
+
+	//Play game
+	const playMatch = () => {
+		const options = document.querySelectorAll('.options button');
+		const playerHand = document.querySelector('player-hand');
+		const computerHand = document.querySelector('computer-hand');
+
+		// Computer options
+		const computerOptions = ['rock', 'paper', 'scissors'];
+
+		options.forEach(option => {
+			option.addEventListener('click', function() {
+				//computer choice
+				const computerNumber = Math.floor(Math.random() * 3);
+				const computerChoice = computerOptions[computerNumber]
+			});
 		});
 	};
 
 	startGame();
+	playMatch();
 	// updateScore();
 };
 
