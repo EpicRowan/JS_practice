95a132fb6370c0e14b3d84e4ee24e8a8e5d0a798
Rowan <shepherd.rowan.k@gmail.com>
2021-05-19 16:18:30 -0700

    Set up JS page for creating add item functionality

---
+++ To_do_Project/app.js
@@ -0,0 +1,12 @@
+// Grab DOM elements
+const todo_items = document.querySelector('.todo-items')
+
+// Event Listeners
+
+todo_items.addEventListener('click', addToDo)
+
+// Functions 
+
+function addToDo(event) {
+    
+}
