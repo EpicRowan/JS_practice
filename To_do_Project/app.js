9e35e0a801e500f227c9ec0176589d37a9e953e6
Rowan <shepherd.rowan.k@gmail.com>
2021-05-19 17:13:14 -0700

    Added prevent default for function

--- To_do_Project/app.js
+++ To_do_Project/app.js
@@ -1,12 +1,17 @@
 // Grab DOM elements
-const todo_items = document.querySelector('.todo-items')
+const todo_items = document.querySelector('.todo-items');
+const todo_button = document.querySelector('.todo-button');
 
 // Event Listeners
 
-todo_items.addEventListener('click', addToDo)
+todo_button.addEventListener('click', addToDo);
 
 // Functions 
 
 function addToDo(event) {
-    
+    //prevent form from submitting
+    event.preventDefault();
+
+    console.log("I'm working");
+
 }
