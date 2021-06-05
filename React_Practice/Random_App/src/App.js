92be753835273a4f68cda6f28a44d0110af75c6a
Rowan <shepherd.rowan.k@gmail.com>
2021-06-04 23:50:35 -0700

    Setting up javascript sandbox app.js and index.html

--- React_Practice/Random_App/src/App.js
+++ React_Practice/Random_App/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Nav from './Components/Nav';
 
 function App() {
   return (
     <div className="App">
-
+      <h1> Hello World</h1>
+      <Nav name = {"Bob"}/>
     </div>
   );
 }
